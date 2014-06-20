ad852bf0a29f01023f6da392a3875182e7463f66
rkoeninger <rkoeninger@att.net>
2014-06-20 16:47:09 -0400

    Major re-writing (tests fail)

--- src/revolt.clj
+++ src/revolt.clj
@@ -1,12 +1,104 @@
-
 (ns revolt)
+
 (use 'clojure.set)
 (use 'clojure.pprint)
 
-(defn map-vals [f map] (reduce (fn [m2 [k v]] (assoc m2 k (f v))) {} map))
+(defrecord Bid [gold blackmail force])
+(defn +bid [x y] (merge-with + x y))
+(def has-blackmail? (comp pos? :blackmail))
+(def has-force? (comp pos? :force))
+(defn zero-bid? [bid] (every? zero? (map bid [:gold :blackmail :force])))
+(defn compare-bid [x y]
+    (cond (> (:force x) (:force y)) x
+          (> (:force y) (:force x)) y
+          (> (:blackmail x) (:blackmail y)) x
+          (> (:blackmail y) (:blackmail x)) y
+          (> (:gold x) (:gold y)) x
+          (> (:gold y) (:gold x)) y
+          (->Bid 0 0 0)))
 
-(defn clear-banks [board] 
-  (update-in board [:player-bank] (partial map-vals (fn [_] [0 0 0]))))
+(defrecord Location [support influence-limit])
+
+(defrecord Figure [support bank immunities influence-location special])
+(def blackmail-immune? (comp :blackmail :immunities))
+(def force-immune? (comp :force :immunities))
+
+(defrecord Player [id])
+
+(defrecord Board [locations figures players banks influence support])
+(defn clear-banks [board] (assoc board :banks (zipmap players (repeat (->Bid 0 0 0)))))
+(defn occupied-influence [board location] (reduce + (vals (get-in board [:influence location]))))
+(defn location-full? [board location] (>= (occupied-influence board location) (:influence-limit influence)))
+(defn board-full? [board] (every? (partial location-full? board) (:locations board)))
+(defn get-influence [board location player] (get-in board [:influence location player]))
+(defn has-influence [board location player] (not (zero? (get-in board [:influence location player]))))
+(defn add-support [board player amount] (update-in board [:support player] (partial + amount)))
+(defn add-bank [board player bank] (update-in board [:banks player] (partial +bid bank)))
+(defn add-influence [board location player]
+    (assert (not (location-full? board location)) (str location " already full"))
+    (update-in board [:influence location player] (partial + 1)))
+(defn remove-influence [board location player]
+    (assert (has-influence board location player) (str player " has no influence on " location))
+    (update-in board [:influence location player] (comp (partial max 0) dec)))
+(defn move-influence [board location0 location1 player]
+    (-> board (remove-influence player location0) (add-influence player location1)))
+(defn replace-influence [board location player0 player1]
+    (-> board (remove-influence player0 location) (add-influence player1 location)))
+(defn swap-influence [board location0 location1 player0 player1]
+    (-> board (replace-influence player0 player1 location1) (replace-influence player1 player0 location0)))
+
+(defn set-guard-house [board player] (assoc board :guard-house player))
+(def prompt-spy nil)
+(def prompt-apothecary nil)
+(def prompt-messenger nil)
+(def prompt-mayor nil)
+
+(defmacro deflocation [sym support influence-limit]
+    `(def ~sym (->Location ~support ~influence-limit)))
+
+(deflocation tavern     20 4)
+(deflocation market     25 5)
+(deflocation plantation 30 6)
+(deflocation cathedral  35 7)
+(deflocation harbor     40 6)
+(deflocation town-hall  45 7)
+(deflocation fortress   50 8)
+(deflocation palace     55 8)
+
+(defmacro deffigure [sym support bank-vec immunities & [influence-location special]]
+    `(def ~sym (->Figure ~support (apply ->Bid ~bank-vec) immunities influence-location special)))
+
+(def -- #{})
+(def b- #{:blackmail})
+(def -f #{:force})
+(def bf #{:blackmail :force})
+
+(deffigure printer    10 [0 0 0] --)
+(deffigure constable  5  [0 1 0] bf)
+(deffigure rogue      0  [0 2 0] bf)
+(deffigure mercenary  0  [0 0 1] bf)
+(deffigure general    1  [0 0 1] -f fortress)
+(deffigure captain    1  [0 0 1] -f harbor)
+(deffigure innkeeper  3  [0 1 0] b- tavern)
+(deffigure magistrate 1  [0 1 0] b- town-hall)
+(deffigure priest     6  [0 0 0] -- cathedral)
+(deffigure aristocrat 5  [3 0 0] -- plantation)
+(deffigure merchant   3  [5 0 0] -- market)
+(deffigure viceroy    0  [0 0 0] -- palace set-guard-house)
+(deffigure spy        0  [0 0 0] b- nil prompt-spy)
+(deffigure apothecary 0  [0 0 0] -f nil prompt-apothecary)
+(deffigure messenger  3  [0 0 0] -- nil prompt-messenger)
+(deffigure mayor      0  [0 0 0] bf nil prompt-mayor)
+
+
+
+
+
+
+
+
+
+(defn map-vals [f map] (reduce (fn [m2 [k v]] (assoc m2 k (f v))) {} map))
 
 (defn fill-banks [board]
   (update-in board [:player-bank] (partial map-vals (fn [bank]
@@ -15,61 +107,6 @@
         [(+ c (- 5 token-count)) b f]
         bank))))))
 
-(defn add-sup [board player amount]
-  (update-in board [:player-sup player] (partial + amount)))
-
-(defn +nil [x y] (+ (or x 0) (or y 0)))
-
-(defn +bank [[c1 b1 f1] [c2 b2 f2]] [(+nil c1 c2) (+nil b1 b2) (+nil f1 f2)])
-
-(defn add-bank [board player bank]
-  (update-in board [:player-bank player] (partial +bank bank)))
-
-(defn loc-full? [board loc]
-  (>=
-    (reduce + (vals (get-in board [:player-inf loc])))
-    (get-in board [:locs loc :inf-limit])))
-
-(defn board-full? [board]
-  (every? (partial loc-full? board) (keys (board :locs))))
-
-(defn add-inf [board player loc]
-  (if (loc-full? board loc)
-    (throw (Exception. (str loc " is already full")))
-    (update-in board [:player-inf loc player] (partial + 1))))
-
-(defn rem-inf [board player loc]
-  (if (zero? (get-in board [:player-inf loc player]))
-    (throw (Exception. (str player " has no influence on " loc)))
-    (update-in board [:player-inf loc player] #(max 0 (- %1 1)))))
-
-(defn move-inf [board player loc-from loc-to]
-  (-> board (rem-inf player loc-from) (add-inf player loc-to)))
-
-(defn replace-inf [board player-out player-in loc]
-  (-> board (rem-inf player-out loc) (add-inf player-in loc)))
-
-(defn swap-inf [board p1 loc1 p2 loc2]
-  (-> board (replace-inf p1 p2 loc2) (replace-inf p2 p1 loc1)))
-
-(defn total-inf [board loc]
-  (reduce + (vals (get-in board [:player-inf loc]))))
-
-(defn zero-bid? [x] (or (nil? x) (= x [0 0 0])))
-
-(defn bid-has-blackmail [[c b f]] (and (not (nil? b)) (> b 0)))
-
-(defn bid-has-force [[c b f]] (and (not (nil? f)) (> f 0)))
-
-(defn compare-bids [x y]
-  (let [[c1 b1 f1] x [c2 b2 f2] y]
-    (cond
-      (and (zero-bid? x) (zero-bid? y)) nil
-      (zero-bid? y) x (zero-bid? x) y
-      (> f1 f2) x (< f1 f2) y
-      (> b1 b2) x (< b1 b2) y
-      (> c1 c2) x (< c1 c2) y
-      :else nil)))
 
 (defn inverted-get [map val] ((map-invert map) val))
 
@@ -88,19 +125,6 @@
     (if-not (empty? inf-map)
       (let [max-inf (apply max (vals inf-map))]
         (if (val-unique? inf-map max-inf) (inverted-get inf-map max-inf))))))
-
-(defn print-board [board]
-  (do
-    (println)
-    (println "Guard House:")
-    (pprint (board :guard-house))
-    (println "Support:")
-    (pprint (board :player-sup))
-    (println "Influence:")
-    (pprint (board :player-inf))
-    (println "Bank:")
-    (pprint (board :player-bank))
-    (flush)))
 
 (defn map-conj [maps]
   (cond
@@ -132,76 +156,6 @@
     (<= (count bids) 6)
     (= (get-in board [:player-bank player]) (reduce +bank (vals bids)))
     (every? #(validate-fig-bid board %1 (bids %1)) (keys bids))))
-
-(defn read-bid [board player]
-  (do
-    (println player "has bank =" (get-in board [:player-bank player]))
-    (flush)
-    (let [bids (read-string (read-line))]
-      (if (validate-player-bids board player bids)
-        bids
-        (do (println "invalid bid") (flush) (recur board player))))))
-
-(defn prompt-bids [board]
-  (do
-    (print-board board)
-    (map-conj (map
-      (fn [p] (combine-bid-keys (read-bid board p) p))
-      (keys (board :player-sup))))))
-
-(defn prompt-spy [board winner]
-  (do
-    (println)
-    (println winner "wins the Spy")
-    (print-board board)
-    (println "Pick location, player")
-    (flush)
-    (let [loc (keyword (read-line))
-          player (read-line)]
-      (replace-inf board player winner loc))))
-
-(defn prompt-apothecary [board winner]
-  (do
-    (println)
-    (println winner "wins the Apothecary")
-    (print-board board)
-    (println "Pick location, player, location, player")
-    (flush)
-    (let [loc1 (keyword (read-line))
-          p1 (read-line)
-          loc2 (keyword (read-line))
-          p2 (read-line)]
-      (swap-inf board p1 loc1 p2 loc2))))
-
-(defn prompt-messenger [board winner]
-  (do
-    (println)
-    (println winner "wins the Messenger")
-    (print-board board)
-    (println "Reassign count? (0 - 2) ")
-    (flush)
-    (let [reassign-count (min 2 (max 0 (int (read-line))))]
-      (loop [i 0 b board]
-        (if (< i reassign-count)
-          (do
-            (println "Pick location, location: ")
-            (let [loc-from (keyword (read-line))
-                  loc-to (keyword (read-line))]
-              (recur (inc i) (move-inf b winner loc-from loc-to))))
-          b)))))
-
-(defn prompt-mayor [board winner]
-  (do
-    (println)
-    (println winner "wins the Mayor")
-    (print-board board)
-    (println "Pick location: ")
-    (flush)
-    (let [loc (keyword (read-line))]
-      (add-inf board winner loc))))
-
-(defn set-guard-house [board player]
-  (assoc board :guard-house player))
 
 (defn inc-turn [board]
   (update-in board [:turn] (partial + 1)))
@@ -237,61 +191,9 @@
   (let [max-sup (max (vals player-sup))]
     (if (val-unique? player-sup max-sup) (inverted-get player-sup max-sup))))
 
-(defn run-game [board]
-  (if (board-full? board)
-    (do
-      (println)
-      (println "GAME OVER")
-      (println)
-      (println "The winner is:" (get-game-winner (board :player-sup)))
-      (flush)
-      board)
-    (do
-      (println)
-      (run-game (run-turn board (prompt-bids board))))))
-
-(defn make-loc [sup inf] {:sup-val sup, :inf-limit inf})
-
-(defn make-fig [sup bank b-immune f-immune & [loc special]] {
-  :sup-val sup
-  :bank-val bank
-  :b-immune b-immune
-  :f-immune f-immune
-  :inf-loc loc
-  :special special
-})
-
 (defn make-board [players]
   (let [
       init-bank [3 1 1]
-      locs {
-        :tavern     (make-loc 20 4)
-        :market     (make-loc 25 5)
-        :plantation (make-loc 30 6)
-        :cathedral  (make-loc 35 7)
-        :harbor     (make-loc 40 6)
-        :town-hall  (make-loc 45 7)
-        :fortress   (make-loc 50 8)
-        :palace     (make-loc 55 8)
-      }
-      figs {
-        :printer    (make-fig 10 [0 0 0] false false)
-        :constable  (make-fig 5  [0 1 0] true  true)
-        :rogue      (make-fig 0  [0 2 0] true  true)
-        :mercenary  (make-fig 0  [0 0 1] true  true)
-        :general    (make-fig 1  [0 0 1] false true  :fortress)
-        :captain    (make-fig 1  [0 0 1] false true  :harbor)
-        :innkeeper  (make-fig 3  [0 1 0] true  false :tavern)
-        :magistrate (make-fig 1  [0 1 0] true  false :town-hall)
-        :priest     (make-fig 6  [0 0 0] false false :cathedral)
-        :aristocrat (make-fig 5  [3 0 0] false false :plantation)
-        :merchant   (make-fig 3  [5 0 0] false false :market)
-        :viceroy    (make-fig 0  [0 0 0] false false :palace set-guard-house)
-        :spy        (make-fig 0  [0 0 0] true  false nil prompt-spy)
-        :apothecary (make-fig 0  [0 0 0] false true  nil prompt-apothecary)
-        :messenger  (make-fig 3  [0 0 0] false false nil prompt-messenger)
-        :mayor      (make-fig 0  [0 0 0] true  true  nil prompt-mayor)
-      }
       fig-eval-order [
         :general       :captain       :innkeeper     :magistrate
         :viceroy       :priest        :aristocrat    :merchant
@@ -311,9 +213,3 @@
       :figs figs
       :fig-eval-order fig-eval-order
     }))
-
-(defn play-game []
-  (do
-    (println "Enter players names: ")
-    (flush)
-    (run-game (make-board (read-string (str "[" (read-line) "]"))))))
