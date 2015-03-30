55b866aa027ff3804639bfb60330ed818a213bce
rkoeninger <rkoeninger@att.net>
2015-03-30 13:39:10 -0400

    Refactored revolt-server tests

--- test/revolt_server_test.clj
+++ test/revolt_server_test.clj
@@ -1,67 +1,28 @@
 (ns revolt-server-test
-	(:use revolt)
-	(:use revolt-server)
-	(:use clojure.test))
+    (:use revolt)
+    (:use revolt-server)
+    (:use clojure.test))
 
 (deftest signup-rejected-after-game-has-started
-	(let [board-atom (atom nil)
-		  player-ids-atom (atom #{})
-		  sender-channel nil
-		  transmit-responses (atom [])
-		  broadcast-responses (atom [])
-		  clear-responses (fn [] (do (reset! transmit-responses []))
-		  	                         (reset! broadcast-responses []))
-		  transmit (fn [_ message] (swap! transmit-responses conj message))
-		  broadcast (fn [message] (swap! broadcast-responses conj message))]
-		(handle-message sender-channel
-			            {:user-name "rob"
-			             :content {:type :signup}}
-			            transmit
-			            broadcast
-			            board-atom
-			            nil
-			            player-ids-atom)
-		(is (= #{"rob"} @player-ids-atom))
-		(is (= [{:content :signup-accepted :user-name "rob"}] @broadcast-responses))
-		(clear-responses)
-		(handle-message sender-channel
-			            {:user-name "joe"
-			             :content {:type :signup}}
-			            transmit
-			            broadcast
-			            board-atom
-			            nil
-			            player-ids-atom)
-		(is (= #{"rob" "joe"} @player-ids-atom))
-		(is (= [{:content :signup-accepted :user-name "joe"}] @broadcast-responses))
-		(clear-responses)
-		(handle-message sender-channel
-                        {:user-name "rob"
-                         :content {:type :start-game}}
-                        transmit
-                        broadcast
-                        board-atom
-                        nil
-                        player-ids-atom)
-        (is @board-atom)
-        (clear-responses)
-        (handle-message sender-channel
-                        {:user-name "moe"
-                         :content {:type :signup}}
-                        transmit
-                        broadcast
-                        board-atom
-                        nil
-                        player-ids-atom)
-        (is (= #{"rob" "joe"} @player-ids-atom))
+    (let [board (atom nil)
+          player-ids (atom #{})
+          transmit-responses (atom [])
+          broadcast-responses (atom [])
+          transmit #(swap! transmit-responses conj %2)
+          broadcast #(swap! broadcast-responses conj %)
+          handle #(do (reset! transmit-responses [])
+                      (reset! broadcast-responses [])
+                      (handle-message nil % transmit broadcast board nil player-ids))]
+        (handle {:user-name "rob" :content {:type :signup}})
+        (is (= #{"rob"} @player-ids))
+        (is (= [{:content :signup-accepted :user-name "rob"}] @broadcast-responses))
+        (handle {:user-name "joe" :content {:type :signup}})
+        (is (= #{"rob" "joe"} @player-ids))
+        (is (= [{:content :signup-accepted :user-name "joe"}] @broadcast-responses))
+        (handle {:user-name "rob" :content {:type :start-game}})
+        (is @board)
+        (handle {:user-name "moe" :content {:type :signup}})
+        (is (= #{"rob" "joe"} @player-ids))
         (is (= [{:content :game-already-started}] @transmit-responses))
-        (clear-responses)
-        (handle-message sender-channel
-                        {:user-name "moe"
-                         :content {:type :start-game}}
-                        transmit
-                        broadcast
-                        board-atom
-                        nil
-                        player-ids-atom)
+        (handle {:user-name "moe" :content {:type :start-game}})
         (is (= [{:content :game-already-started}] @transmit-responses))))
