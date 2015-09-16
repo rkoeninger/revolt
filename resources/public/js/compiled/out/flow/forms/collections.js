00116a7d56b616701913fe485020dac9fa37eafe
rkoeninger <rkoeninger@att.net>
2015-09-16 14:23:48 -0400

    Removed om-tut, removed compiled/out folder

--- resources/public/js/compiled/out/flow/forms/collections.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2850 {}
-goog.provide('flow.forms.collections');
-goog.require('cljs.core');
-
-//# sourceMappingURL=collections.js.map?rel=1442373855038
--- resources/public/js/compiled/out/flow/state.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 0.0-2850 {}
-goog.provide('flow.state');
-goog.require('cljs.core');
-flow.state._STAR_state_STAR_ = cljs.core.PersistentArrayMap.EMPTY;
-
-//# sourceMappingURL=state.js.map?rel=1442373853742
