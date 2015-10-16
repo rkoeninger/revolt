e96f5d6db8b40648fff858790fe5c6b1d0c6d0c4
rkoeninger <rkoeninger@att.net>
2015-10-16 16:39:34 -0400

    Re-indented phantom test scripts to 2 spaces

--- scenario_helpers.js
+++ scenario_helpers.js
@@ -1,94 +1,98 @@
+'use strict';
+
+/*jslint node: true, browser: true, indent: 2*/
+/*global phantom*/
 
 function step(delay, func) {
-    return {
-        delay: delay,
-        func: func
-    };
+  return {
+    delay: delay,
+    func: func
+  };
 }
 
 function sequence(steps) {
-    var current, rest;
-    if (steps && steps.length > 0) {
-        current = steps[0];
-        rest = steps.slice(1, steps.length);
-        setTimeout(function () {
-            current.func();
-            sequence(rest);
-        }, current.delay);
-    }
+  var current, rest;
+  if (steps && steps.length > 0) {
+    current = steps[0];
+    rest = steps.slice(1, steps.length);
+    setTimeout(function () {
+      current.func();
+      sequence(rest);
+    }, current.delay);
+  }
 }
 
 function defer(f, args) {
-    return function () {
-        return f.apply(this, args || []);
-    };
+  return function () {
+    return f.apply(this, args || []);
+  };
 }
 
 function repeatAction(n, f) {
-    var i;
-    for (i = 0; i < n; ++i) {
-        f();
-    }
+  var i;
+  for (i = 0; i < n; ++i) {
+    f();
+  }
 }
 
 function signup(username) {
-    var signupInput = document.body.querySelector("#signup-input"),
-        signupButton = document.body.querySelector("#signup-button");
-    if (signupInput) {
-        if (signupButton) {
-            signupInput.value = username;
-            signupButton.click();
-        } else {
-            console.error("Couldn't find #signup-button");
-        }
+  var signupInput = document.body.querySelector("#signup-input"),
+      signupButton = document.body.querySelector("#signup-button");
+  if (signupInput) {
+    if (signupButton) {
+      signupInput.value = username;
+      signupButton.click();
     } else {
-        console.error("Couldn't find #signup-input");
+      console.error("Couldn't find #signup-button");
     }
+  } else {
+    console.error("Couldn't find #signup-input");
+  }
 }
 
 function startGame() {
-    var startGameButton = document.body.querySelector("#start-game-button");
-    if (startGameButton) {
-        startGameButton.click();
-    } else {
-        console.error("Couldn't find #start-game-button");
-    }
+  var startGameButton = document.body.querySelector("#start-game-button");
+  if (startGameButton) {
+    startGameButton.click();
+  } else {
+    console.error("Couldn't find #start-game-button");
+  }
 }
 
 function incBid(figure, denomination) {
-    var incButtonId = "bid-" + figure + "-" + denomination + "-up",
-        incButton = document.body.querySelector("#" + incButtonId);
-    if (incButton) {
-        incButton.click();
-    } else {
-        console.error("Couldn't find #" + incButtonId);
-    }
+  var incButtonId = "bid-" + figure + "-" + denomination + "-up",
+      incButton = document.body.querySelector("#" + incButtonId);
+  if (incButton) {
+    incButton.click();
+  } else {
+    console.error("Couldn't find #" + incButtonId);
+  }
 }
 
 function bid(figure, gold, blackmail, force) {
-    return {
-        figure: figure,
-        gold: gold,
-        blackmail: blackmail,
-        force: force
-    };
+  return {
+    figure: figure,
+    gold: gold,
+    blackmail: blackmail,
+    force: force
+  };
 }
 
 function placeBids(bids) {
-    var i, j, current;
-    for (i = 0; i < bids.length; ++i) {
-        current = bids[i];
-        repeatAction(current.gold, defer(incBid, [current.figure, "gold"]));
-        repeatAction(current.blackmail, defer(incBid, [current.figure, "blackmail"]));
-        repeatAction(current.force, defer(incBid, [current.figure, "force"]));
-    }
+  var i, j, current;
+  for (i = 0; i < bids.length; ++i) {
+    current = bids[i];
+    repeatAction(current.gold, defer(incBid, [current.figure, "gold"]));
+    repeatAction(current.blackmail, defer(incBid, [current.figure, "blackmail"]));
+    repeatAction(current.force, defer(incBid, [current.figure, "force"]));
+  }
 }
 
 function submitBids() {
-    var submitButton = document.body.querySelector("#submit-button");
-    if (submitButton) {
-        submitButton.click();
-    } else {
-        console.error("Couldn't find #submit-button");
-    }
+  var submitButton = document.body.querySelector("#submit-button");
+  if (submitButton) {
+    submitButton.click();
+  } else {
+    console.error("Couldn't find #submit-button");
+  }
 }
--- scenarios.js
+++ scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/*jslint node: true, browser: true, indent: 4*/
+/*jslint node: true, browser: true, indent: 2*/
 /*global phantom*/
 
 var url = "http://localhost:3449/";
@@ -11,53 +11,53 @@ var webpage = require("webpage"),
     page2 = webpage.create();
 
 page1.onConsoleMessage = function (msg) {
-    console.log(msg);
+  console.log(msg);
 };
 page2.onConsoleMessage = function (msg) {
-    console.log(msg);
+  console.log(msg);
 };
 
 page1.onInitialized = function () {
-    page1.injectJs(helpersUrl);
+  page1.injectJs(helpersUrl);
 };
 page2.onInitialized = function () {
-    page2.injectJs(helpersUrl);
+  page2.injectJs(helpersUrl);
 };
 
 page1.open(url, function (status) {
-    if (status !== "success") {
-        console.log("Unable to access test site");
-        phantom.exit(1);
-    } else {
-        page1.evaluate(function () {
-            sequence([
-                step(4000, defer(signup, ["emma"])),
-                step(8000, defer(placeBids, [[
-                    bid("priest",     1, 0, 0),
-                    bid("aristocrat", 0, 1, 0),
-                    bid("merchant",   1, 0, 0),
-                    bid("viceroy",    1, 0, 1)]])),
-                step(1000, defer(submitBids))
-            ]);
-        });
-    }
+  if (status !== "success") {
+    console.log("Unable to access test site");
+    phantom.exit(1);
+  } else {
+    page1.evaluate(function () {
+      sequence([
+        step(4000, defer(signup, ["emma"])),
+        step(8000, defer(placeBids, [[
+          bid("priest",     1, 0, 0),
+          bid("aristocrat", 0, 1, 0),
+          bid("merchant",   1, 0, 0),
+          bid("viceroy",    1, 0, 1)]])),
+        step(2000, defer(submitBids))
+      ]);
+    });
+  }
 });
 page2.open(url, function (status) {
-    if (status !== "success") {
-        console.log("Unable to access test site");
-        phantom.exit(1);
-    } else {
-        page2.evaluate(function () {
-            sequence([
-                step(4000, defer(signup, ["noah"])),
-                step(4000, defer(startGame)),
-                step(4000, defer(placeBids, [[
-                    bid("general",    2, 0, 0),
-                    bid("captain",    0, 1, 0),
-                    bid("innkeeper",  0, 0, 1),
-                    bid("magistrate", 1, 0, 0)]])),
-                step(1000, defer(submitBids))
-            ]);
-        });
-    }
+  if (status !== "success") {
+    console.log("Unable to access test site");
+    phantom.exit(1);
+  } else {
+    page2.evaluate(function () {
+      sequence([
+        step(4000, defer(signup, ["noah"])),
+        step(4000, defer(startGame)),
+        step(4000, defer(placeBids, [[
+          bid("general",    2, 0, 0),
+          bid("captain",    0, 1, 0),
+          bid("innkeeper",  0, 0, 1),
+          bid("magistrate", 1, 0, 0)]])),
+        step(2000, defer(submitBids))
+      ]);
+    });
+  }
 });
