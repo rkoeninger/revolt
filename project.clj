bb0f6897efdf9074b01c61b0bd9e3ef1f8c860ff
rkoeninger <rkoeninger@att.net>
2016-09-26 01:28:53 -0400

    Updated dependencies

--- project.clj
+++ project.clj
@@ -30,25 +30,25 @@
           :pretty-print false}}}
     :test-commands {"test" ["phantomjs" "scenarios.js"]}}
   :plugins [
-    [lein-cljsbuild "1.1.0"]
-    [lein-figwheel "0.4.0"]]
+    [lein-cljsbuild "1.1.4"]
+    [lein-figwheel "0.5.7"]]
   :dependencies [
-    [org.clojure/clojure "1.7.0"]
-    [org.clojure/clojurescript "1.7.122"]
-    [org.clojure/math.combinatorics "0.1.1"]
-    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-    [jarohen/chord "0.6.0"]
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/clojurescript "1.9.229"]
+    [org.clojure/math.combinatorics "0.1.3"]
+    [org.clojure/core.async "0.2.391"]
+    [jarohen/chord "0.7.0"]
     [hiccup "1.0.5"]
     [hyjinks "0.0.2-SNAPSHOT"]
-    [ring/ring-core "1.4.0"]
-    [ring-middleware-format "0.6.0"]
-    [compojure "1.4.0"]
+    [ring/ring-core "1.5.0"]
+    [ring-middleware-format "0.7.0"]
+    [compojure "1.5.1"]
     [com.cemerick/url "0.1.1"]
-    [figwheel "0.4.0"]
+    [figwheel "0.5.7"]
     [org.omcljs/om "0.9.0" :exclusions [cljsjs/react]]
-    [cljs-react-test "0.1.3-SNAPSHOT"]
+    [cljs-react-test "0.1.4-SNAPSHOT"]
     [cljsjs/react-with-addons "0.13.3-0"]
-    [prismatic/om-tools "0.3.12"]]
+    [prismatic/om-tools "0.4.0"]]
   :exclusions
     [org.clojure/clojure]
   :figwheel {
